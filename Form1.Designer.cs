e1033e32afbb4419315359e248bf751129062bdb
Xinyou Han <left@whu.edu.cn>
2016-10-25 20:41:40 +0800

    add prj sources

---
+++ Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace n1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boxAmount = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtTango = new System.Windows.Forms.Label();
+            this.txtCurrent = new System.Windows.Forms.Label();
+            this.txtOf = new System.Windows.Forms.Label();
+            this.txtAll = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.stsScore = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.ckLog = new System.Windows.Forms.CheckBox();
+            this.ckFav = new System.Windows.Forms.CheckBox();
+            this.ckD = new System.Windows.Forms.RadioButton();
+            this.ckC = new System.Windows.Forms.RadioButton();
+            this.ckA = new System.Windows.Forms.RadioButton();
+            this.ckB = new System.Windows.Forms.RadioButton();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // boxAmount
+            // 
+            this.boxAmount.Location = new System.Drawing.Point(184, 13);
+            this.boxAmount.Name = "boxAmount";
+            this.boxAmount.Size = new System.Drawing.Size(100, 21);
+            this.boxAmount.TabIndex = 0;
+            this.boxAmount.Text = "100";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(290, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtTango
+            // 
+            this.txtTango.AutoSize = true;
+            this.txtTango.Font = new System.Drawing.Font("Yu Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtTango.Location = new System.Drawing.Point(237, 44);
+            this.txtTango.Name = "txtTango";
+            this.txtTango.Size = new System.Drawing.Size(62, 25);
+            this.txtTango.TabIndex = 2;
+            this.txtTango.Text = "tango";
+            // 
+            // txtCurrent
+            // 
+            this.txtCurrent.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtCurrent.AutoSize = true;
+            this.txtCurrent.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtCurrent.Location = new System.Drawing.Point(430, 49);
+            this.txtCurrent.Name = "txtCurrent";
+            this.txtCurrent.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtCurrent.Size = new System.Drawing.Size(16, 16);
+            this.txtCurrent.TabIndex = 3;
+            this.txtCurrent.Text = "0";
+            this.txtCurrent.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtOf
+            // 
+            this.txtOf.AutoSize = true;
+            this.txtOf.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtOf.Location = new System.Drawing.Point(457, 49);
+            this.txtOf.Name = "txtOf";
+            this.txtOf.Size = new System.Drawing.Size(16, 16);
+            this.txtOf.TabIndex = 4;
+            this.txtOf.Text = "/";
+            // 
+            // txtAll
+            // 
+            this.txtAll.AutoSize = true;
+            this.txtAll.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtAll.Location = new System.Drawing.Point(469, 49);
+            this.txtAll.Name = "txtAll";
+            this.txtAll.Size = new System.Drawing.Size(16, 16);
+            this.txtAll.TabIndex = 5;
+            this.txtAll.Text = "0";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stsScore});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 256);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(589, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // stsScore
+            // 
+            this.stsScore.Name = "stsScore";
+            this.stsScore.Size = new System.Drawing.Size(52, 17);
+            this.stsScore.Text = "Right: 0";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(511, 45);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(50, 23);
+            this.btnNext.TabIndex = 11;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Visible = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // ckLog
+            // 
+            this.ckLog.AutoSize = true;
+            this.ckLog.Checked = true;
+            this.ckLog.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckLog.Location = new System.Drawing.Point(392, 15);
+            this.ckLog.Name = "ckLog";
+            this.ckLog.Size = new System.Drawing.Size(42, 16);
+            this.ckLog.TabIndex = 12;
+            this.ckLog.Text = "Log";
+            this.ckLog.UseVisualStyleBackColor = true;
+            // 
+            // ckFav
+            // 
+            this.ckFav.AutoSize = true;
+            this.ckFav.Location = new System.Drawing.Point(461, 15);
+            this.ckFav.Name = "ckFav";
+            this.ckFav.Size = new System.Drawing.Size(72, 16);
+            this.ckFav.TabIndex = 13;
+            this.ckFav.Text = "Favorite";
+            this.ckFav.UseVisualStyleBackColor = true;
+            // 
+            // ckD
+            // 
+            this.ckD.AutoSize = true;
+            this.ckD.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ckD.Location = new System.Drawing.Point(12, 200);
+            this.ckD.Name = "ckD";
+            this.ckD.Size = new System.Drawing.Size(34, 20);
+            this.ckD.TabIndex = 9;
+            this.ckD.TabStop = true;
+            this.ckD.Tag = "item";
+            this.ckD.Text = "D";
+            this.ckD.UseVisualStyleBackColor = true;
+            this.ckD.Visible = false;
+            this.ckD.CheckedChanged += new System.EventHandler(this.ckD_CheckedChanged);
+            // 
+            // ckC
+            // 
+            this.ckC.AutoSize = true;
+            this.ckC.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ckC.Location = new System.Drawing.Point(12, 160);
+            this.ckC.Name = "ckC";
+            this.ckC.Size = new System.Drawing.Size(34, 20);
+            this.ckC.TabIndex = 8;
+            this.ckC.TabStop = true;
+            this.ckC.Tag = "item";
+            this.ckC.Text = "C";
+            this.ckC.UseVisualStyleBackColor = true;
+            this.ckC.Visible = false;
+            this.ckC.CheckedChanged += new System.EventHandler(this.ckC_CheckedChanged);
+            // 
+            // ckA
+            // 
+            this.ckA.AllowDrop = true;
+            this.ckA.AutoSize = true;
+            this.ckA.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ckA.Location = new System.Drawing.Point(12, 80);
+            this.ckA.Name = "ckA";
+            this.ckA.Size = new System.Drawing.Size(34, 20);
+            this.ckA.TabIndex = 6;
+            this.ckA.TabStop = true;
+            this.ckA.Tag = "item";
+            this.ckA.Text = "A";
+            this.ckA.UseVisualStyleBackColor = true;
+            this.ckA.Visible = false;
+            this.ckA.CheckedChanged += new System.EventHandler(this.ckA_CheckedChanged);
+            // 
+            // ckB
+            // 
+            this.ckB.AutoSize = true;
+            this.ckB.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ckB.Location = new System.Drawing.Point(12, 120);
+            this.ckB.Name = "ckB";
+            this.ckB.Size = new System.Drawing.Size(34, 20);
+            this.ckB.TabIndex = 7;
+            this.ckB.TabStop = true;
+            this.ckB.Tag = "item";
+            this.ckB.Text = "B";
+            this.ckB.UseVisualStyleBackColor = true;
+            this.ckB.Visible = false;
+            this.ckB.CheckedChanged += new System.EventHandler(this.ckB_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 278);
+            this.Controls.Add(this.ckB);
+            this.Controls.Add(this.ckA);
+            this.Controls.Add(this.ckC);
+            this.Controls.Add(this.ckFav);
+            this.Controls.Add(this.ckD);
+            this.Controls.Add(this.ckLog);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtAll);
+            this.Controls.Add(this.txtOf);
+            this.Controls.Add(this.txtCurrent);
+            this.Controls.Add(this.txtTango);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.boxAmount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox boxAmount;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label txtTango;
+        private System.Windows.Forms.Label txtCurrent;
+        private System.Windows.Forms.Label txtOf;
+        private System.Windows.Forms.Label txtAll;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel stsScore;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.CheckBox ckLog;
+        private System.Windows.Forms.CheckBox ckFav;
+        private System.Windows.Forms.RadioButton ckD;
+        private System.Windows.Forms.RadioButton ckC;
+        private System.Windows.Forms.RadioButton ckA;
+        private System.Windows.Forms.RadioButton ckB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace n1
+{
+    public partial class Form1 : Form
+    {
+        Random tangoRand = new Random();
+        //int wa,wa2;
+        //string va,va2;
+        string fav;
+        string temp;
+        //allTango为每行组成的字符串数组
+        string[] allTango, favSet, tempTango;
+        //tango为单行拆开后的字符串数组，重复使用
+        string[] tango = new string[6];
+        int tangoAmount, testAmount, right, track;
+        int i, answer;
+        Color dftCol = Color.FromName("control");
+        Color ansCol = Color.FromName("LightGreen");
+        RadioButton[] options = new RadioButton[4];
+
+
+        //tangoIndex是所有元素（全部或fav）的索引
+        //index是单次循环中随机提取的4个元素的索引
+        int[] tangoIndex, index;
+        //int[] optIndex = { 1, 2, 3, 4 };
+        char[] delimLine = { '\n' };
+        char[] delimField = { ',' };
+        public Form1()
+        {
+            InitializeComponent();
+            options[0] = ckA;
+            options[1] = ckB;
+            options[2] = ckC;
+            options[3] = ckD;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            boxAmount.Enabled = false;
+            ckLog.Enabled = false;
+            ckFav.Enabled = false;
+            ckA.Visible = true;
+            ckB.Visible = true;
+            ckC.Visible = true;
+            ckD.Visible = true;
+            btnNext.Visible = true;
+
+            System.IO.StreamReader fileTango = new System.IO.StreamReader(@"D:\N1.txt");
+            temp = fileTango.ReadToEnd();
+            fileTango.Close();
+            allTango = temp.Split(delimLine);
+            tangoAmount = allTango.Length;
+
+            track = 1;
+            right = 0;
+            stsScore.Text = "Right: " + right.ToString();
+
+            //勾选fav，则读取fav.txt的内容（单行数字组成的逗号分隔符文件）
+            //同时tangoIndex的元素就是fav的单个数字
+            //默认fav中的数字总数不小于4
+            if (ckFav.Checked)
+            {
+                System.IO.StreamReader fileFav = new System.IO.StreamReader(@"D:\fav.txt");
+                fav = fileFav.ReadLine();
+                favSet = fav.Split(delimField);
+                fileFav.Close();
+                tangoIndex = new int[favSet.Length];
+                for (i = 0; i < favSet.Length; i++)
+                    tangoIndex[i] = Convert.ToInt16(favSet[i]);
+            }
+            //不勾选fav，tangoIndex的元素就是0到allTango.Length-1的序数
+            else
+            {
+                tangoIndex = new int[allTango.Length];
+                for (i = 0; i < allTango.Length; i++)
+                    tangoIndex[i] = i;
+            }
+            
+            tangoAmount = tangoIndex.Length;
+
+            testAmount = Convert.ToInt16(boxAmount.Text);
+            txtAll.Text = testAmount.ToString();
+            txtCurrent.Text = "1";
+            //每次循环，从tangoIndex里面随机提取4个元素，作为索引
+            //单次提取的4个元素不重复，多次循环中提取的元素允许重复
+            //用tempTango代替allTango进行操作，避免allTango元素被修改
+            tempTango = new string[allTango.Length];
+            allTango.CopyTo(tempTango, 0);
+            //tempTango = allTango;
+            index = new int[4];
+            for (i = 0; i < 4; i++)
+            {
+                index[i] = tangoRand.Next(0, tangoAmount - i);
+                tango = tempTango[tangoIndex[index[i]]].Split(delimField);
+                tempTango[tangoIndex[index[i]]] = tempTango[tangoIndex[tangoAmount - 1 - i]];
+                options[i].Text = tango[3];
+            }
+            answer = tangoRand.Next(0, 4);
+            txtTango.Text = allTango[tangoIndex[index[answer]]].Split(delimField)[0];
+        }
+
+        private void ckA_CheckedChanged(object sender, EventArgs e)
+        {
+            options[answer].BackColor = ansCol;
+            ckA.Enabled = false;
+            ckB.Enabled = false;
+            ckC.Enabled = false;
+            ckD.Enabled = false;
+            if (answer == 0 && ckA.Checked)
+            {
+                right++;
+                stsScore.Text = "Right: " + right.ToString();
+            }
+        }
+
+        private void ckB_CheckedChanged(object sender, EventArgs e)
+        {
+            options[answer].BackColor = ansCol;
+            ckA.Enabled = false;
+            ckB.Enabled = false;
+            ckC.Enabled = false;
+            ckD.Enabled = false;
+            if (answer == 1 && ckB.Checked)
+            {
+                right++;
+                stsScore.Text = "Right: " + right.ToString();
+            }
+        }
+
+        private void ckC_CheckedChanged(object sender, EventArgs e)
+        {
+            options[answer].BackColor = ansCol;
+            ckA.Enabled = false;
+            ckB.Enabled = false;
+            ckC.Enabled = false;
+            ckD.Enabled = false;
+            if (answer == 2 && ckC.Checked)
+            {
+                right++;
+                stsScore.Text = "Right: " + right.ToString();
+            }
+        }
+
+        private void ckD_CheckedChanged(object sender, EventArgs e)
+        {
+            options[answer].BackColor = ansCol;
+            ckA.Enabled = false;
+            ckB.Enabled = false;
+            ckC.Enabled = false;
+            ckD.Enabled = false;
+            if (answer == 3 && ckD.Checked)
+            {
+                right++;
+                stsScore.Text = "Right: " + right.ToString();
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (track < testAmount)
+            {
+                track++;
+                txtCurrent.Text = track.ToString();
+
+                //还原颜色
+                ckA.Checked = false;
+                ckB.Checked = false;
+                ckC.Checked = false;
+                ckD.Checked = false;
+                ckA.Enabled = true;
+                ckB.Enabled = true;
+                ckC.Enabled = true;
+                ckD.Enabled = true;
+                ckA.BackColor = dftCol;
+                ckB.BackColor = dftCol;
+                ckC.BackColor = dftCol;
+                ckD.BackColor = dftCol;
+
+                //重置tempTango
+                allTango.CopyTo(tempTango, 0);
+                for (i = 0; i < 4; i++)
+                {
+                    index[i] = tangoRand.Next(0, tangoAmount - i);
+                    tango = tempTango[tangoIndex[index[i]]].Split(delimField);
+                    tempTango[tangoIndex[index[i]]] = tempTango[tangoIndex[tangoAmount - 1 - i]];
+                    options[i].Text = tango[0];
+                }
+                answer = tangoRand.Next(0, 4);
+                txtTango.Text = allTango[tangoIndex[index[answer]]].Split(delimField)[0];
+            }
+        }
+
+
+       
+    }
+}
